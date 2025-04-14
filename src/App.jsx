6516d5bf3845ff5db2c166363a8df13cb0ca5c6a
Antoni Kreatif <74832010+soratakeita@users.noreply.github.com>
2025-04-15 05:53:00 +0700

    update bang

--- src/App.jsx
+++ src/App.jsx
@@ -8,9 +8,6 @@ function App() {
     <>
     <Header />
     <Home />
-  <h1 class="min-h-screen flex items-center justify-center bg-white dark:bg-gray-900 text-gray-900 dark:text-white">
-    Vocapraya website!
-  </h1>
     </>
   )
 }
